b671527af8822c57e35ee0115a8a9b146bd0eaa9
Niilo Ursin <niilo.ursin@gmail.com>
2019-10-10 22:45:42 +0300

    document functions

--- lib/voikko.d.ts
+++ lib/voikko.d.ts
@@ -18,6 +18,32 @@ export interface Analysis {
   WORDBASES: string;
 }
 
+export interface Token {
+  type: TokenType;
+  text: string;
+}
+
+export enum TokenType {
+  WORD = 'WORD',
+  WHITESPACE = 'WHITESPACE',
+  PUNCTUATION = 'PUNCTUATION',
+}
+
+export interface Sentence {
+  text: string;
+  nextStartType: NextStartType;
+}
+
+export enum NextStartType {
+  PROBABLE = 'PROBABLE',
+  NONE = 'NONE',
+}
+
+export enum SuggestionStrategy {
+  OCR = 'OCR',
+  TYPO = 'TYPO',
+}
+
 export interface Voikko {
   /**
    * Releases the resources allocated by libvoikko for this instance.
@@ -25,17 +51,22 @@ export interface Voikko {
   terminate: () => void;
 
   /**
-   * Check the spelling of given word. Return true if the word is correct, false if it is incorrect.
+   * Check the spelling of given word. Return true if the word is correct,
+   * false if it is incorrect.
    */
   spell: (word: string) => boolean;
 
   /**
-   * a list of suggested spellings for given (misspelled) word. If the given word is correct, the list contains only the word itself
+   * a list of suggested spellings for given (misspelled) word. If the given
+   * word is correct, the list contains only the word itself
    */
   suggest: (word: string) => Array<string>;
 
   /**
-   * Check the given text for grammar errors and return a list of GrammarError objects representing the errors that were found. Unlike the C based API this method accepts multiple paragraphs separated by newline characters.
+   * Check the given text for grammar errors and return a list of
+   * GrammarError objects representing the errors that were found.
+   * Unlike the C based API this method accepts multiple paragraphs
+   * separated by newline characters.
    */
   grammarErrors: (text: string, language?: string) => Array<GrammarError>;
 
@@ -45,79 +76,128 @@ export interface Voikko {
   analyze: (word: string) => Array<Analysis>;
 
   /**
-   *
+   * Split the given natural language text into a list of Token objects.
    */
-  tokens: () => void;
+  tokens: (text: string) => Array<Token>;
+
   /**
-   *
+   * Split the given natural language text into a list of Sentence objects.
    */
-  sentences: () => void;
+  sentences: (text: string) => Array<Sentence>;
+
   /**
+   * Return a character pattern that describes the hyphenation of given word.
    *
+   * ' ' = no hyphenation at this character,
+   * '-' = hyphenation point (character at this position is preserved in the hyphenated form),
+   * '=' = hyphentation point (character at this position is replaced by the hyphen.)
    */
-  getHyphenationPattern: () => void;
+  getHyphenationPattern: (word: string) => string;
+
   /**
-   *
+   * Returns the given word in fully hyphenated form.
    */
-  hyphenate: () => void;
+  hyphenate: (word: string) => string;
+
   /**
-   *
+   * Ignore dot at the end of the word (needed for use in some word processors).
+   * If this option is set and input word ends with a dot, spell checking and
+   * hyphenation functions try to analyze the word without the dot if no
+   * results can be obtained for the original form. Also with this option,
+   * string tokenizer will consider trailing dot of a word to be a part of
+   * that word. Default: false
    */
-  setIgnoreDot: () => void;
+  setIgnoreDot: (value: boolean) => void;
+
   /**
-   *
+   * Ignore words containing numbers. Default: false
    */
-  setIgnoreNumbers: () => void;
+  setIgnoreNumbers: (value: boolean) => void;
+
   /**
-   *
+   * Accept words that are written completely in uppercase letters without
+   * checking them at all. Default: false
    */
-  setIgnoreUppercase: () => void;
+  setIgnoreUppercase: (value: boolean) => void;
+
   /**
-   *
+   * Accept words even when the first letter is in uppercase
+   * (start of sentence etc.) Default: true
    */
-  setAcceptFirstUppercase: () => void;
+  setAcceptFirstUppercase: (value: boolean) => void;
+
   /**
-   *
+   * Accept words even when all of the letters are in uppercase.
+   * Note that this is not the same as setIgnoreUppercase: with this option
+   * the word is still checked, only case differences are ignored. Default: true
    */
-  setAcceptAllUppercase: () => void;
+  setAcceptAllUppercase: (value: boolean) => void;
+
   /**
-   *
+   * (Spell checking only): Ignore non-words such as URLs and email addresses.
+   * Default: true
    */
-  setIgnoreNonwords: () => void;
+  setIgnoreNonwords: (value: boolean) => void;
+
   /**
-   *
+   * (Spell checking only): Allow some extra hyphens in words. This option
+   * relaxes hyphen checking rules to work around some unresolved issues in
+   * the underlying morphology, but it may cause some incorrect words to
+   * be accepted. The exact behaviour (if any) of this option is not specified.
+   * Default: false
    */
-  setAcceptExtraHyphens: () => void;
+  setAcceptExtraHyphens: (value: boolean) => void;
+
   /**
-   *
+   * (Spell checking only): Accept missing hyphens at the start and end of
+   * the word. Some application programs do not consider hyphens to be word
+   * characters. This is reasonable assumption for many languages but not
+   * for Finnish. If the application cannot be fixed to use proper tokenization
+   * algorithm for Finnish, this option may be used to tell libvoikko to work
+   * around this defect. Default: false
    */
-  setAcceptMissingHyphens: () => void;
+  setAcceptMissingHyphens: (value: boolean) => void;
+
   /**
-   *
+   * (Grammar checking only): Accept incomplete sentences that could occur in
+   * titles or headings. Set this option to true if your application is not
+   * able to differentiate titles from normal text paragraphs, or if you know
+   * that you are checking title text. Default: false
    */
-  setAcceptTitlesInGc: () => void;
+  setAcceptTitlesInGc: (value: boolean) => void;
+
   /**
-   *
+   * (Grammar checking only): Accept incomplete sentences at the end of the
+   * paragraph. These may exist when text is still being written. Default: false
    */
-  setAcceptUnfinishedParagraphsInGc: () => void;
+  setAcceptUnfinishedParagraphsInGc: (value: boolean) => void;
+
   /**
-   *
+   * (Grammar checking only): Accept paragraphs if they would be valid within
+   * bulleted lists. Default: false
    */
-  setAcceptBulletedListsInGc: () => void;
+  setAcceptBulletedListsInGc: (value: boolean) => void;
+
   /**
-   *
+   * Do not insert hyphenation positions that are considered to be ugly but
+   * correct Default: false
    */
-  setNoUglyHyphenation: () => void;
+  setNoUglyHyphenation: (value: boolean) => void;
+
   /**
-   *
+   * (Hyphenation only): Hyphenate unknown words. Default: true
    */
-  setHyphenateUnknownWords: () => void;
+  setHyphenateUnknownWords: (value: boolean) => void;
+
   /**
-   *
+   * The minimum length for words that may be hyphenated. This limit is also
+   * enforced on individual parts of compound words. Default: 2
    */
-  setMinHyphenatedWordLength: () => void;
+  setMinHyphenatedWordLength: (value: boolean) => void;
+
   /**
-   *
+   * Set the suggestion strategy to be used when generating spelling
+   * suggestions. Default: SuggestionStrategy.TYPO
    */
-  setSuggestionStrategy: () => void;
+  setSuggestionStrategy: (suggestionStrategy: SuggestionStrategy) => void;
 }
